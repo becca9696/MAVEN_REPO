c7170feee6fbb68e2655bd498afc976b77a02df7
Luca Beccarini <61837048+becca9696@users.noreply.github.com>
2020-05-05 20:38:33 +0200

    Add files via upload

---
+++ src/main/java/it/esempioMaven/mavenArt02/App.java
@@ -0,0 +1,16 @@
+package it.esempioMaven.mavenArt02;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        
+    	Hello hello = new Hello();
+    	hello.foo();
+    	
+    }
+}
---
+++ src/main/java/it/esempioMaven/mavenArt02/Hello.java
@@ -0,0 +1,11 @@
+package it.esempioMaven.mavenArt02;
+
+public class Hello {
+	
+	public void foo() {
+		
+		System.out.println("HI! We are using MAVEN|");
+		
+	}
+
+}
